46e86697df786dc54aab74ff6172fe5b8c45202f
Sebas1705 <sebssgarcia502580@gmail.com>
2023-02-04 14:10:11 +0100

    Añadido ejemplos de html y clases

--- profesores_a_casa/src/main/java/es/codeurjc/dad/profesores_a_casa/Controller/HomeController.java
+++ profesores_a_casa/src/main/java/es/codeurjc/dad/profesores_a_casa/Controller/HomeController.java
@@ -1,18 +1,30 @@
 package es.codeurjc.dad.profesores_a_casa.controller;
 
 import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import es.codeurjc.dad.profesores_a_casa.service.ExampleService;
+
 @Controller
 public class HomeController {
     
+    @Autowired
+    private ExampleService exampleService;
+
     @GetMapping("/")
     public String home(Model model, HttpSession sesion){
         sesion.getId();
         return "index";
     }
 
+    @GetMapping("/pagina")
+    public String pagina(Model model){
+        model.addAttribute("hola","Buenas");
+        return "pagina";
+    }
 }
 
