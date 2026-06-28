465359adfec342be84d3e10c07566ebc66f23849
Sebastián Ramiro Entrerrios García <100527906+Sebas1705@users.noreply.github.com>
2026-06-28 14:54:09 +0200

    i18n: translate all email bodies and method names to English

--- servicio_correo/src/main/java/es/codeurjc/dad/servicio_correo/service/RabbitMQConsumer.java
+++ servicio_correo/src/main/java/es/codeurjc/dad/servicio_correo/service/RabbitMQConsumer.java
@@ -24,96 +24,102 @@ public class RabbitMQConsumer {
     @Value("${mail.password}")
     private String password;
 
-    private final String textFinal=
-        "Te deseamos una buena estancia y te animamos a "+
-        "que añadas tu también tus propios anuncios!";
+    private final String textFinal =
+        "We wish you a great experience and encourage you to " +
+        "post your own offers too!";
 
     private static final Logger log = LoggerFactory.getLogger(RabbitMQConsumer.class);
 
     @RabbitListener(queues="${rabbitmq.queue.name}")
     public void consume(String message){
-        log.info(String.format("Recived message -> %s", message));
-        String[] mCutted=message.substring(2).split("-");
+        log.info(String.format("Received message -> %s", message));
+        String[] parts = message.substring(2).split("-");
         switch(message.charAt(0)){
-            //Nuevo contrato:
+            // New contract:
             case 'N':
-                log.info("Caso N");
-                enviarConGMail(mCutted[0],"Nuevo contrato",
-                            "Has creado con éxito el contrato "+mCutted[1]+
-                            ", entre tu y el profesor "+mCutted[2]+" del post "+
-                            "con titulo "+mCutted[3]+".\n"+textFinal);
+                log.info("Case N: new contract");
+                sendWithGMail(parts[0], "New contract",
+                            "You have successfully created a contract " + parts[1] +
+                            " between you and teacher " + parts[2] +
+                            " for the post \"" + parts[3] + "\"." +
+                            "\n" + textFinal);
                 break;
-            //Borrar Contrato:
+            // Cancel contract:
             case 'C':
-                log.info("Caso C");
-                enviarConGMail(mCutted[0],"Contrato borrado",
-                            "Se ha borrado con éxito el contrato "+mCutted[1]+
-                            ", entre tu y el profesor "+mCutted[2]+" del post "+
-                            "con titulo "+mCutted[3]+".\n"+textFinal);
+                log.info("Case C: contract cancelled");
+                sendWithGMail(parts[0], "Contract cancelled",
+                            "The contract " + parts[1] +
+                            " between you and teacher " + parts[2] +
+                            " for the post \"" + parts[3] + "\" has been successfully cancelled." +
+                            "\n" + textFinal);
                 break;
-            //Nuevo Post:
+            // New post:
             case 'I':
-                log.info("Caso I");
-                enviarConGMail(mCutted[0],"Nuevo post",
-                            "Has creado con éxito el post con titulo "+
-                            mCutted[1]+", descripcion \""+mCutted[2]+"\" "+
-                            "y precio de "+mCutted[3]+" euros.\n"+textFinal);
+                log.info("Case I: new post");
+                sendWithGMail(parts[0], "New post published",
+                            "You have successfully published the post \"" + parts[1] +
+                            "\" with description \"" + parts[2] + "\" " +
+                            "and price " + parts[3] + " EUR." +
+                            "\n" + textFinal);
                 break;
-            //Borrar Post:
+            // Post deleted:
             case 'P':
-                log.info("Caso P");
-                enviarConGMail(mCutted[0],"Post borrado",
-                            "Has borrado con éxito el post con titulo "+mCutted[1]+".\n"+textFinal);
+                log.info("Case P: post deleted");
+                sendWithGMail(parts[0], "Post deleted",
+                            "You have successfully deleted the post \"" + parts[1] + "\"." +
+                            "\n" + textFinal);
                 break;
-            //Borrar Usuario:
+            // User deleted:
             case 'D':
-                log.info("Caso D");
-                enviarConGMail(mCutted[0],"Usuario borrado",
-                            "Has borrado con éxito el usuario llamado "+mCutted[1]+".\n"+textFinal);
+                log.info("Case D: user deleted");
+                sendWithGMail(parts[0], "Account deleted",
+                            "The account \"" + parts[1] + "\" has been successfully deleted." +
+                            "\n" + textFinal);
                 break;
-            //Registro:
+            // Registration:
             case 'S':
-                log.info("Caso S");
-                enviarConGMail(mCutted[0],"Usuario creado",
-                            "Has logrado registrarte adecuadamente el usuario"+
-                            " con nombre "+mCutted[1]+".\n"+textFinal);
+                log.info("Case S: user registered");
+                sendWithGMail(parts[0], "Account created",
+                            "You have successfully registered the account \"" + parts[1] + "\"." +
+                            "\n" + textFinal);
                 break;
-            //Nuevo reporte:
+            // New report:
             case 'R':
-                log.info("Caso R");
-                enviarConGMail(message,"Nuevo reporte",
-                            "Has hecho un reporte al post "+mCutted[1]+
-                            ", con motivo \""+mCutted[2]+"\" y descripcion \""+
-                            mCutted[3]+"\".\n"+textFinal);
+                log.info("Case R: new report");
+                sendWithGMail(message, "New report submitted",
+                            "You have submitted a report on post \"" + parts[1] +
+                            "\" with reason \"" + parts[2] +
+                            "\" and description \"" + parts[3] + "\"." +
+                            "\n" + textFinal);
                 break;
             default:
-                log.info("Not valid message");
+                log.info("Not a valid message type");
         }
     }
 
-    private void enviarConGMail(String destinatario, String asunto, String cuerpo) {
-        
+    private void sendWithGMail(String recipient, String subject, String body) {
+
         Properties props = System.getProperties();
-        props.put("mail.smtp.host", "smtp.gmail.com");  //El servidor SMTP de Google
+        props.put("mail.smtp.host", "smtp.gmail.com");  // Google SMTP server
         props.put("mail.smtp.user", origin);
-        props.put("mail.smtp.clave", password);    //La clave de la cuenta
-        props.put("mail.smtp.auth", "true");    //Usar autenticación mediante usuario y clave
-        props.put("mail.smtp.starttls.enable", "true"); //Para conectar de manera segura al servidor SMTP
-        props.put("mail.smtp.port", "587"); //El puerto SMTP seguro de Google
-    
+        props.put("mail.smtp.clave", password);
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.port", "587");  // Google secure SMTP port
+
         Session session = Session.getDefaultInstance(props);
         MimeMessage message = new MimeMessage(session);
-    
+
         try {
             message.setFrom(new InternetAddress(origin));
-            message.addRecipient(Message.RecipientType.TO, new InternetAddress(destinatario));   //Se podrían añadir varios de la misma manera
-            message.setSubject(asunto);
-            message.setText(cuerpo);
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
+            message.setSubject(subject);
+            message.setText(body);
             Transport transport = session.getTransport("smtp");
-            transport.connect("smtp.gmail.com",origin,password);
+            transport.connect("smtp.gmail.com", origin, password);
             transport.sendMessage(message, message.getAllRecipients());
             transport.close();
         }
-        catch (MessagingException me) {me.printStackTrace();}
+        catch (MessagingException me) { me.printStackTrace(); }
     }
 }
